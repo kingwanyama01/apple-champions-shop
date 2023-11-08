14ef8ec11970c51fd8444387ee1387d03dbaeb65
kingwanyama01 <kingwanyama01@gmail.com>
2023-11-08 16:16:11 +0300

    Finished working on auth app

--- appleChampionsShop/settings.py
+++ appleChampionsShop/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'appleChampionsShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'champions_shop',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -128,3 +132,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home-url'
+LOGIN_URL = 'login-url'
